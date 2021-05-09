f19af33a1cdbd626f7c8b1ba0fc74691616d3bd5
Quang Hy Le <lequanghy1998@yahoo.ca>
2021-05-09 12:27:35 -0400

    Add Login Function

--- app/src/main/java/com/to/hacks/MainActivity.java
+++ app/src/main/java/com/to/hacks/MainActivity.java
@@ -1,8 +1,8 @@
 package com.to.hacks;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
